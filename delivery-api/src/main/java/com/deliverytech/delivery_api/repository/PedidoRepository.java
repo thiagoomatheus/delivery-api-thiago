7581a75eb362b5732d857e4da13320e36d136654
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-07-03 14:43:21 +0000

    corrigir erros PedidoRepository

--- delivery-api/src/main/java/com/deliverytech/delivery_api/repository/PedidoRepository.java
+++ delivery-api/src/main/java/com/deliverytech/delivery_api/repository/PedidoRepository.java
@@ -2,5 +2,10 @@ package com.deliverytech.delivery_api.repository;
 
 import com.deliverytech.delivery_api.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.deliverytech.delivery_api.entity.Cliente;
+import com.deliverytech.delivery_api.entity.OrderStatus; 
 
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {}
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+	List<Pedido> findByCliente(Cliente cliente);
+	List<Pedido> findByStatus(OrderStatus status);
+}
--- delivery-api/src/main/java/com/deliverytech/delivery_api/repository/RestauranteRepository.java
+++ delivery-api/src/main/java/com/deliverytech/delivery_api/repository/RestauranteRepository.java
@@ -4,7 +4,6 @@ import com.deliverytech.delivery_api.entity.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
