aff57c9e14c61bcdad806af851c1a5d419ce8f1a
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-07-23 16:25:11 -0300

    Remover comentário

--- src/main/java/com/deliverytech/delivery_api/model/Restaurante.java
+++ src/main/java/com/deliverytech/delivery_api/model/Restaurante.java
@@ -3,7 +3,6 @@ package com.deliverytech.delivery_api.model;
 import jakarta.persistence.*;
 import lombok.*;
 import java.math.BigDecimal;
-//import java.time.LocalTime;
 import java.util.List;
 
 @Entity
